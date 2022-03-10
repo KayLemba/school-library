11d2fe83dc261243d4a1819bdd86daeb813e467d
KayLemba <kaylemba@yahoo.com>
2022-03-10 13:44:18 +0200

    Make changes as requested by code review

--- person.rb
+++ person.rb
@@ -1,14 +1,15 @@
 require_relative 'corrector'
 
 class Person
-  attr_accessor :name, :age
-  attr_reader :id, :rentals
+  attr_accessor :name, :age, :id
+  attr_reader :rentals, :parent_permission
 
-  def initialize(age:, parent_permission: true, name: 'Unknown')
+  def initialize(age:, name: 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @corrector = Corrector.new
     @rentals = []
   end
 
@@ -16,28 +17,19 @@ class Person
     of_age? || @parent_permission
   end
 
-  def validate_name
-    corrector = Corrector.new
-    @name = corrector.correct_name @name
-  end
-
-  private
-
   def of_age?
     @age >= 18
   end
 
-  def to_s
-    "Name: #{@name}, ID: #{@id}, Age: #{@age}"
+  def validate_name?
+    @name = @corrector.correct_name(@name)
   end
 
-  def to_json(*args)
-    {
-      JSON.create_id => self.class.name,
-      'id' => @id,
-      'name' => @name,
-      'age' => @age,
-      'parent_permission' => @parent_permission
-    }.to_json(*args)
+  def add_rental(rental)
+    @rentals << rental
+  end
+
+  def to_s
+    "Name: #{@name}, ID: #{@id}, Age: #{@age} "
   end
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,14 +1,59 @@
 require_relative '../person'
 
-describe Person do
-  context 'When creating a person' do
-    person = Person.new(age: 30, parent_permission: false, name: 'Susan')
-    it 'Creates a new person with name Susan' do
-      expect(person).to be_an_instance_of Person
+describe 'Person' do
+  before :each do
+    @person = Person.new(age: 20, name: 'Susan', parent_permission: true)
+  end
+
+  describe '#new' do
+    person = Person.new(age: 15)
+
+    it 'return the person object' do
+      expect(@person).to be_an_instance_of Person
     end
 
-    it 'Age of new pwerson to equal 30' do
-      expect(person.age).to eq 30
+    it 'has a set name Susan' do
+      expect(@person.name).to eql 'Susan'
+    end
+
+    it 'has a default name of Unknown' do
+      expect(person.name).to eql 'Unknown'
+    end
+
+    it 'should have parents permission' do
+      expect(person.parent_permission).to be true
+    end
+
+    it 'should not have parents permission' do
+      nperson = Person.new(age: 15, parent_permission: false)
+      expect(nperson.parent_permission).to be_falsey
+    end
+  end
+
+  describe 'Can use services' do
+    context 'is minor' do
+      it 'cannot use services' do
+        person = Person.new(age: 15, name: 'Mithi', parent_permission: false)
+        expect(person.can_use_services?).to be false
+      end
+    end
+    context 'is adult' do
+      it 'can use services' do
+        person = Person.new(age: 18, name: 'Mithi')
+        expect(person.can_use_services?).to be true
+      end
+    end
+    context 'use method to validate age' do
+      it 'is of age' do
+        person = Person.new(age: 18)
+        expect(person.of_age?).to be true
+      end
+    end
+    context 'use method to correct name' do
+      it 'correct name' do
+        person = Person.new(age: 18, name: 'Thisus averylongname')
+        expect(person.validate_name?).to eq('Thisus ave')
+      end
     end
   end
 end
